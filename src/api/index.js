683646f7d6424d12a90f6bc6912e56f32a0a674a
Nick Ray <nickrray@gmail.com>
2020-08-04 14:54:22 -0500

    :recycle: Use env variable for API key

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default {
   async index() {
-    const res = await fetch('https://api.themoviedb.org/3/movie/550?api_key=5107c68304ebe2230239976ae7a05c92')
+    const res = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=${process.env.REACT_APP_API_KEY}`)
     console.log('res')
     console.log(res)
     return await res.json()
