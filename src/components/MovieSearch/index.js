dea6befc7c0462d303f6cfd449b6b31ab4f52986
Nick Ray <nickrray@gmail.com>
2020-08-04 15:59:34 -0500

    :sparkles: Set state from api call results using search input

--- src/components/MovieSearch/index.js
+++ src/components/MovieSearch/index.js
@@ -8,6 +8,20 @@ import { Form } from './Form'
 export const MovieSearch = () => {
   const [movies, setMovies] = useState([])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    // Grab input field
+    const input = e.target.querySelector('input')
+    // Get value from input field
+    const searchText = input.value
+    // Call API with input value
+    const searchedMovies = await api.index(searchText)
+    // Set movies with API response
+    setMovies(searchedMovies.results)
+    // Reset search input
+    input.value = ''
+  }
+
   return (
     <main>
       <Form />
