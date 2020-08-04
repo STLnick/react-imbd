29565baa8b9322cd71bc2bab24575b9e60e3de20
Nick Ray <nickrray@gmail.com>
2020-08-04 17:56:28 -0500

    :sparkles: Add functions to utilize api further

--- src/api/index.js
+++ src/api/index.js
@@ -5,8 +5,25 @@ dotenv.config()
 export default {
   async index(query) {
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&page=1&include_adult=false`)
-    console.log('res')
-    console.log(res)
+
+    return await res.json()
+  },
+
+  async details(id) {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+
+    return await res.json()
+  },
+
+  async upcoming() {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
+
+    return await res.json()
+  },
+
+  async recommended(id) {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
+
     return await res.json()
   }
 }
