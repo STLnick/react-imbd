cd48bc9b50492ca30c94e84ac32c9ba629157480
Nick Ray <nickrray@gmail.com>
2020-08-04 14:36:29 -0500

    :bug: Await the fetch call

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 export default {
   async index() {
-    const res = fetch('https://api.themoviedb.org/3/movie/550?api_key=5107c68304ebe2230239976ae7a05c92')
+    const res = await fetch('https://api.themoviedb.org/3/movie/550?api_key=5107c68304ebe2230239976ae7a05c92')
+    console.log('res')
+    console.log(res)
     return await res.json()
   }
 }
