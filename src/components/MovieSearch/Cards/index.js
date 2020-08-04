00992f65104c4ee3507bc87f9d7ec9ffbe8c2664
Nick Ray <nickrray@gmail.com>
2020-08-04 15:58:38 -0500

    :sparkles: Render cards dynamically

--- src/components/MovieSearch/Cards/index.js
+++ src/components/MovieSearch/Cards/index.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const Cards = () => {
+import './Cards.css'
+
+export const Cards = ({ movies }) => {
+
+  const renderCards = () => {
+    return movies.map(movie => {
+      return (
+        <div key={movie.id} className="card flex flex--column flex--align-center flex--justify-evenly">
+          <h2>{movie.title}</h2>
+          <p>{movie.overview}</p>
+          <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt="Movie poster" />
+        </div>
+      )
+    })
+  }
 
   return (
-    <p>CARDS</p>
+    <div className="cards-container flex flex--wrap flex--justify-evenly">
+      {renderCards()}
+    </div>
   )
+}
+
+Cards.propTypes = {
+  movies: PropTypes.array
 }
