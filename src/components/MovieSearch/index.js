500dfdb5d62089226d16efdab9142ed7492363cd
Nick Ray <nickrray@gmail.com>
2020-08-04 14:36:49 -0500

    :construction: Set movies state with api.index()

--- src/components/MovieSearch/index.js
+++ src/components/MovieSearch/index.js
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from 'react'
 
+import api from 'api'
+
 import { Cards } from './Cards'
 import { Form } from './Form'
 
 export const MovieSearch = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const fetchedMovies = await api.index()
+      setMovies(fetchedMovies)
+    })()
+  }, [])
+
   return (
     <main>
       <Form />
