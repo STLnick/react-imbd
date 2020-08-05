0dd8ecc68510741f0dc30b5b8c0ac65cb516828d
Nick Ray <nickrray@gmail.com>
2020-08-05 14:09:42 -0500

    :art: Extract base url into const to reduce repetition

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,13 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const BASE_URL = 'https://api.themoviedb.org/3'
+
 export default {
 
   async index(query) {
     try {
-      const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&page=1&include_adult=false`)
+      const res = await fetch(`${BASE_URL}/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&page=1&include_adult=false`)
 
       if (res.status > 400) {
         throw new Error(res.status)
@@ -20,7 +22,7 @@ export default {
 
   async details(id) {
     try {
-      const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+      const res = await fetch(`${BASE_URL}/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
 
       return await res.json()
     } catch (error) {
@@ -31,7 +33,7 @@ export default {
 
   async upcoming() {
     try {
-      const res = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
+      const res = await fetch(`${BASE_URL}/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
 
       return await res.json()
     } catch (error) {
@@ -41,7 +43,7 @@ export default {
 
   async recommended(id) {
     try {
-      const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
+      const res = await fetch(`${BASE_URL}/movie/${id}/recommendations?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
 
       return await res.json()
     } catch (error) {
