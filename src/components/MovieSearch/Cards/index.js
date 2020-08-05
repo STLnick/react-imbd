5b259619b2dc2e80d055723f9b507b482bf44260
Nick Ray <nickrray@gmail.com>
2020-08-05 14:10:11 -0500

    :lipstick: Import the modularized CSS

--- src/components/MovieSearch/Cards/index.js
+++ src/components/MovieSearch/Cards/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import './Cards.css'
+import styles from './Cards.module.css'
 
 export const Cards = ({ buttonHandlers, movies }) => {
 
