8828f91095f70cca84c86f94e66640043f3551ce
Nick Ray <nickrray@gmail.com>
2020-08-05 13:07:42 -0500

    :art: Place search button next to search input

--- src/components/MovieSearch/Form/index.js
+++ src/components/MovieSearch/Form/index.js
@@ -7,13 +7,15 @@ export const Form = ({ handlers }) => {
 
   return (
     <form onSubmit={handlers.submit} className="flex flex--column flex--align-center flex--justify-center">
-      <input type="search" placeholder="Find Movies..." />
+      <div className="flex">
+        <input type="search" placeholder="Find Movies..." />
+        <button type="submit">Search</button>
+      </div>
       <input disabled type="number" placeholder="Release Year..." />
       <div className="flex flex--justify-center flex--align-center">
         <label htmlFor="year-filter">Show Movies up to entered year only</label>
         <input id="year-filter" onChange={handlers.checked} type="checkbox" />
       </div>
-      <button type="submit">Search</button>
       <button type="button" onClick={handlers.upcoming}>See Upcoming Movies</button>
     </form>
   )
