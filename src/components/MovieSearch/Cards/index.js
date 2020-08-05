0f7d91048dc1f0c3cd5f295802252118bb10bb93
Nick Ray <nickrray@gmail.com>
2020-08-05 13:08:35 -0500

    :sparkles: Create formatBudget function and implement

--- src/components/MovieSearch/Cards/index.js
+++ src/components/MovieSearch/Cards/index.js
@@ -5,14 +5,45 @@ import './Cards.css'
 
 export const Cards = ({ buttonHandlers, movies }) => {
 
+  const getFormattedBudget = (budget) => {
+    let formattedBudget = '$'
+
+    const chunksOfThree = Math.floor(budget.length / 3)
+
+    if (budget.length % 3 === 0) {
+      let j = 0
+      for (let i = 0; i < chunksOfThree; i++) {
+        formattedBudget += budget.slice(j, j + 3) + (i === chunksOfThree - 1 ? '' : ',')
+        j += 3
+      }
+    } else if (budget.length % 3 === 1) {
+      let j = 1
+      formattedBudget += budget.slice(0, j) + ','
+      for (let i = 0; i < chunksOfThree; i++) {
+        formattedBudget += budget.slice(j, j + 3) + (i === chunksOfThree - 1 ? '' : ',')
+        j += 3
+      }
+    } else {
+      let j = 2
+      formattedBudget += budget.slice(0, j) + ','
+      for (let i = 0; i < chunksOfThree; i++) {
+        formattedBudget += budget.slice(j, j + 3) + (i === chunksOfThree - 1 ? '' : ',')
+        j += 3
+      }
+    }
+
+    return formattedBudget
+  }
+
   const renderCards = () => {
     return movies.map(movie => {
 
       return (
         <div key={movie.id} className="card flex flex--column flex--align-center flex--justify-evenly">
-          <h2>{movie.title} aaaa  </h2>
-          {movie.tagline ? <p>{movie.tagline}</p> : ''}
+          <h2>{movie.title}</h2>
+          {movie.tagline ? <blockquote>{movie.tagline}</blockquote> : ''}
           <p><strong>Released:</strong> {movie.release_date ? movie.release_date : 'TBD'}</p>
+          {movie.budget ? <p><strong>Budget:</strong> {getFormattedBudget(movie.budget.toString())}</p> : ''}
           <p>{movie.overview}</p>
           <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt="Movie poster" />
           <button data-id={movie.id} onClick={buttonHandlers.details} type="button">Get Movie Details</button>
