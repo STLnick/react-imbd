0af225b8a2b66fdfe6f7fbb50e77cd1a692f977b
Nick Ray <nickrray@gmail.com>
2020-08-17 17:04:14 -0500

    :sparkles: Get movie details working using server

--- server/server.js
+++ server/server.js
@@ -8,16 +8,38 @@ dotenv.config();
 const port = 5000;
 
 const server = http.createServer(async (req, res) => {
-  const queryText = url.parse(req.url).query.split('=')[1];
-  const { body } = await got(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${queryText}&page=1&include_adult=false`);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/JSON');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.end(body);
+  const [path, query] = req.url.split('?');
+  let data;
+
+  switch (path) {
+    case '/':
+      const queryText = url.parse(req.url).query.split('=')[1];
+      data = await got(`${process.env.BASE_URL}/search/movie?api_key=${process.env.API_KEY}&query=${queryText}&page=1&include_adult=false`);
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/JSON');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.end(data.body);
+      break;
+    case '/details':
+      const id = url.parse(req.url).query.split('=')[1];
+      data = await got(`${process.env.BASE_URL}/movie/${id}?api_key=${process.env.API_KEY}&language=en-US`);
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/JSON');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.end(data.body);
+      break;
+    default:
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.end('<h1>404 Page Not Found</h1>');
+  }
 });
 
 server.listen(port, '127.0.0.1', () => {
   console.log(`server is listening on port ${port}!`);
 });
 
-// /search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}&page=1&include_adult=false`
+// Details fetch: fetch(`${baseUrl}/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
