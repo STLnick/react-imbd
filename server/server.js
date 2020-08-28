408c409bac32053dc4e91507199bf65d15aeb137
Nick Ray <nickrray@gmail.com>
2020-08-27 20:46:44 -0500

    :recycle: Convert server into express server

--- server/server.js
+++ server/server.js
@@ -1,59 +1,22 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
-import http from 'http';
-import got from 'got';
-import querystring from 'querystring';
+import express from 'express';
+
+import movie from './routes/movie';
+import tv from './routes/tv';
 
 dotenv.config();
-const port = 5000;
 
-const server = http.createServer(async (req, res) => {
-  const [path, _] = req.url.split('?');
-  const query = querystring.parse(req.url.slice(req.url.indexOf('?') + 1));
-  let data;
+const app = express();
 
-  switch (path) {
-    case '/':
-      data = await got(`${process.env.BASE_URL}/search/movie?api_key=${process.env.API_KEY}&query=${query.query}&page=1&include_adult=false`);
+app.use(cors());
+app.use(express.json());
 
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/JSON');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.end(data.body);
-      break;
-    case '/details':
-      data = await got(`${process.env.BASE_URL}/movie/${query.id}?api_key=${process.env.API_KEY}&language=en-US`);
+app.use('/movie', movie);
+app.use('/tv', tv);
 
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/JSON');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.end(data.body);
-      break;
-    case '/upcoming':
-      data = await got(`${process.env.BASE_URL}/movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=1`);
-
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/JSON');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.end(data.body);
-      break;
-    case '/recommended':
-      data = await got(`${process.env.BASE_URL}/movie/${query.id}/recommendations?api_key=${process.env.API_KEY}&language=en-US&page=1`);
-
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/JSON');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.end(data.body);
-      break;
-    default:
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/html');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.end('<h1>404 Page Not Found</h1>');
-  }
-});
-
-server.listen(port, '127.0.0.1', () => {
-  console.log(`server is listening on port ${port}!`);
+app.listen(process.env.PORT, () => {
+  console.log(`server is listening on port ${process.env.PORT}!`);
 });
 
 // Details fetch: fetch(`${baseUrl}/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
