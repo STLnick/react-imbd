c6a6ae0656ae9962a32bf816ca000f895acd1646
Nick Ray <nickrray@gmail.com>
2020-08-27 20:20:58 -0500

    :sparkles: Render Header when not on Home view

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, useLocation } from 'react-router-dom'
 
 import { Header } from './components'
 import { Home, Movies, TV } from './views'
@@ -7,21 +7,24 @@ import { Home, Movies, TV } from './views'
 import './App.css';
 
 export const App = () => {
+  const location = useLocation()
+
   return (
-    <div className="page-container flex flex--column flex--align-center flex--justify-center">
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/movies'>
-          <Header />
-          <Movies />
-        </Route>
-        <Route path='/TV'>
-          <Header />
-          <TV />
-        </Route>
-      </Switch>
-    </div>
+    <>
+      {location.pathname === '/' ? null : <Header />}
+      <div className="page-container flex flex--column flex--align-center flex--justify-center">
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/movies'>
+            <Movies />
+          </Route>
+          <Route path='/TV'>
+            <TV />
+          </Route>
+        </Switch>
+      </div>
+    </>
   );
 }
