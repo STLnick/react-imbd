5d075864825828c44c5d67d1c6dcfbca315cc134
Nick Ray <nickrray@gmail.com>
2020-08-27 20:20:28 -0500

    :sparkles: Create header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
+import tvIcon from '../../img/tv.svg'
+import filmIcon from '../../img/film.svg'
+import logo from '../../img/filmRecorder.svg'
+
 export const Header = () => {
   const location = useLocation()
 
   return (
-    <nav>
-      <div className="nav-left">
-        {/* Logo */}
-        <div className="nav-menu">
-          Some stuff here in future...
-        </div>
+    <nav className="top-nav flex flex--align-center flex--justify-between">
+      <div className="nav-left flex flex--justify-center">
+        <img className="logo" src={logo} alt="Film recorder icon" />
+        <h3 class="title">IMDB Movie / TV Search</h3>
       </div>
       <div className="nav-right">
         {location.pathname === '/movies'
-          ? <Link to='/tv'>Search TV Shows</Link>
-          : <Link to='/movies'>Search Movies</Link>}
+          ? <Link className="flex flex--align-center button primary-bg" to='/tv'>
+            <img className="button-svg filter-white" src={tvIcon} alt="TV icon" />
+            Search TV Shows
+          </Link>
+          : <Link className="button primary-bg" to='/movies'>
+            <img className="button-svg filter-white" src={filmIcon} alt="Film icon" />
+            Search Movies
+          </Link>}
       </div>
     </nav>
   )
