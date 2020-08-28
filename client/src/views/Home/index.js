7360d81a2eed8a05ff7ea96266b30660242750c3
Nick Ray <nickrray@gmail.com>
2020-08-27 20:21:16 -0500

    :construction: Setup basic Home view with Links

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import tvIcon from '../../img/tv.svg'
+import filmIcon from '../../img/film.svg'
+
 export const Home = () => {
   return (
     <div>
       <h2>Home Page</h2>
-      <Link to='/movies'>Search Movies</Link>
-      <Link to='/tv'>Search TV Shows</Link>
+      <Link to='/movies'>
+        <img className="button-svg" src={filmIcon} alt="Film icon" />
+        Search Movies
+      </Link>
+      <Link to='/tv'>
+        <img className="button-svg" src={tvIcon} alt="TV icon" />
+        Search TV Shows
+      </Link>
     </div>
   )
 }
