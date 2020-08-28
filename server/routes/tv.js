c9a401711e70b8644d6d75445f43e1744f921956
Nick Ray <nickrray@gmail.com>
2020-08-27 21:14:27 -0500

    :sparkles: Finish TV routes in server
    All routes tested in Postman

--- server/routes/tv.js
+++ server/routes/tv.js
@@ -1,7 +1,35 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import got from 'got';
+import querystring from 'querystring';
+
+dotenv.config();
 
 const router = express.Router();
 
 router.get('/');
+
+router.get('/', async (req, res) => {
+  const query = querystring.parse(req.url.slice(req.url.indexOf('?') + 1));
+  const { body } = await got(`${process.env.BASE_URL}/search/tv?api_key=${process.env.API_KEY}&query=${query.query}&page=1&include_adult=false`);
+  res.status(200).end(body);
+});
+
+router.get('/details', async (req, res) => {
+  const query = querystring.parse(req.url.slice(req.url.indexOf('?') + 1));
+  const { body } = await got(`${process.env.BASE_URL}/tv/${query.id}?api_key=${process.env.API_KEY}&language=en-US`);
+  res.status(200).end(body);
+});
+
+router.get('/upcoming', async (req, res) => {
+  const { body } = await got(`${process.env.BASE_URL}/tv/airing_today?api_key=${process.env.API_KEY}&language=en-US&page=1`);
+  res.status(200).end(body);
+});
+
+router.get('/recommended', async (req, res) => {
+  const query = querystring.parse(req.url.slice(req.url.indexOf('?') + 1));
+  const { body } = await got(`${process.env.BASE_URL}/tv/${query.id}/recommendations?api_key=${process.env.API_KEY}&language=en-US&page=1`);
+  res.status(200).end(body);
+});
 
 export default router;
