70cc0be890aba766ab83a545315548ee2044d9ab
Nick Ray <nickrray@gmail.com>
2020-08-27 21:13:55 -0500

    :sparkles: Finish api TV function factory
    All routes tested in Postman

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -48,6 +48,47 @@ export default {
     }
   }),
   tv: (baseUrl = `http://localhost:${process.env.REACT_APP_PORT}`) => ({
-    // TODO: Build methods to fetch appropriately
+    async index(query) {
+      try {
+        const res = await fetch(`${baseUrl}/tv?query=${query}`)
+
+        if (res.status > 400) {
+          throw new Error(res.status)
+        }
+
+        return await res.json()
+      } catch (error) {
+        console.log(`Currently facing issue with ${error.message}`)
+      }
+    },
+    async details(id) {
+      try {
+        const res = await fetch(`${baseUrl}/tv/details?id=${id}`)
+        // const res = await fetch(`${baseUrl}/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+
+        return await res.json()
+      } catch (error) {
+        console.log(`Currently facing issue with ${error.message}`)
+      }
+
+    },
+    async upcoming() {
+      try {
+        const res = await fetch(`${baseUrl}/tv/upcoming`)
+
+        return await res.json()
+      } catch (error) {
+        console.log(`Currently facing issue with ${error.message}`)
+      }
+    },
+    async recommended(id) {
+      try {
+        const res = await fetch(`${baseUrl}/tv/recommended?id=${id}`)
+
+        return await res.json()
+      } catch (error) {
+        console.log(`Currently facing issue with ${error.message}`)
+      }
+    }
   })
 }
