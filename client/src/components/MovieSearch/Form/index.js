abc3ee0ab182dacc10acc11575c9dc6feb1b590a
Nick Ray <nickrray@gmail.com>
2020-08-27 21:37:00 -0500

    :sparkles: Change Form text depending on current route

--- client/src/components/MovieSearch/Form/index.js
+++ client/src/components/MovieSearch/Form/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import './Form.css'
 
 export const Form = ({ handlers }) => {
+  const location = useLocation()
 
   return (
     <form onSubmit={handlers.submit} className="flex flex--column flex--align-center flex--justify-center">
+      <h3 className="title">
+        {location.pathname === '/movies' ? 'Movies' : 'TV Shows'}
+      </h3>
       <div className="flex">
-        <input type="search" placeholder="Find Movies..." />
+        <input type="search" placeholder={location.pathname === '/movies' ? 'Find Movies...' : 'Find TV Shows...'} />
         <button type="submit">Search</button>
       </div>
 
@@ -19,7 +24,7 @@ export const Form = ({ handlers }) => {
         <input disabled type="number" id="max-year" placeholder="Max Year" />
       </div>
       <div className="flex flex--justify-center flex--align-center">
-        <label className="checkmark-container" htmlFor="year-filter">Filter Movies by Release Year
+        <label className="checkmark-container" htmlFor="year-filter">Filter {location.pathname === '/movies' ? 'Movies' : 'TV Shows'} by Year
           <input id="year-filter" onChange={handlers.checked} type="checkbox" />
           <span className="checkmark"></span>
         </label>
